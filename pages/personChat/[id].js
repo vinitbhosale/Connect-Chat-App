1b6d568eb5241cf497477f95b9aedbb1d06b73b9
Vinit Bhosale <vinitbhosale925@gmail.com>
2021-05-12 11:56:51 -0400

    updates in pages

--- pages/personChat/[id].js
+++ pages/personChat/[id].js
@@ -3,7 +3,7 @@ import { useAuthState } from "react-firebase-hooks/auth";
 
 import styled from "styled-components";
 import Head from "next/head";
-import Sidebar from "../../components/sidebar";
+import Sidebar from "../../components/Sidebar";
 import ChatScreen from "../../components/ChatScreen";
 import getRecipientEmail from "../../utils/getRecipientEmail";
 
